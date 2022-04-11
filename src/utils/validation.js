6bb4946b3c71c2fedfa82069b5e92bf24f817f35
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-04-11 00:54:04 -0400

    bug fixed

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,6 +1,6 @@
 export const isValidLetter = (key) => {
   const isValid =
-    /[a-z]/i.test(key.toString().toLowerCase()) &&
+    /[a-z-ñ]/i.test(key.toString().toLowerCase()) &&
     key.length === 1 // no special keys
   return isValid
 }
