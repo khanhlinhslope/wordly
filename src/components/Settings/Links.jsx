70d917eae97506b664b2a0b4077a92853418531a
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-04-11 00:27:05 -0400

    refactor

---
+++ src/components/Settings/Links.jsx
@@ -0,0 +1,74 @@
+import {
+  Box,
+  Flex,
+  Link,
+  useColorModeValue,
+  Text
+} from '@chakra-ui/react'
+
+const Links = ({ ...props }) => {
+  const links = [
+    {
+      id: 'about',
+      title: 'About',
+      urlCaption: 'Twitter',
+      url: 'https://twitter.com/marsigliaCR'
+    },
+    {
+      id: 'source-code',
+      title: 'Source code',
+      urlCaption: 'GitHub',
+      url: 'https://github.com/marsigliadev/wordly'
+    }
+  ]
+
+  return (
+    <Flex
+      flexDir='column'
+      justify='space-between'
+      align='center'
+      w='100%'
+      {...props}
+    >
+      {links.map(link => {
+        const { id, title, url, urlCaption } = link
+
+        return (
+          <Flex
+            key={id}
+            flexDir='row'
+            justify='space-between'
+            align='center'
+            // h='full'
+            w='100%'
+            borderBottom='1px solid #A0AEC0'
+            px={4}
+            h='75px'
+          >
+            <Box>
+              <Text fontWeight='bold'>
+                {title}
+              </Text>
+            </Box>
+
+            <Flex
+              align='center'
+              color={useColorModeValue('#818692', '#c3c7e0')}
+              fontSize={16}
+            >
+              <Link
+                href={url}
+                isExternal
+                textDecoration='underline'
+              >
+                {urlCaption}
+              </Link>
+            </Flex>
+          </Flex>
+        )
+      })}
+    </Flex>
+  )
+}
+
+export default Links
--- src/components/Settings/Options.jsx
+++ src/components/Settings/Options.jsx
@@ -1,91 +1,13 @@
-import { Flex, Box, FormControl, FormLabel, Switch, Text, useColorModeValue, useColorMode } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import FormOptions from 'components/Settings/Form'
+import Links from 'components/Settings/Links'
 
 const Options = ({ ...props }) => {
-  const { colorMode, toggleColorMode } = useColorMode()
-
-  const items = [
-    {
-      id: 'dark-mode',
-      title: 'Dark mode',
-      subtitle: 'Toggle between dark and light mode.',
-      handler: toggleColorMode,
-      value: colorMode === 'dark',
-      enabled: true
-    },
-    {
-      id: 'blind-mode',
-      title: 'Blind mode',
-      subtitle: 'Toggle between blind and normal mode.',
-      handler: () => { },
-      value: false,
-      enabled: false
-    },
-    {
-      id: 'swap-keys',
-      title: 'Swap special keys',
-      subtitle: 'Swap "Enter" and "Backspace" buttons.',
-      handler: () => { },
-      value: false,
-      enabled: false
-    },
-    {
-      id: 'fat-keys',
-      title: 'Fat fingers',
-      subtitle: 'Increase the size of the buttons.',
-      handler: () => { },
-      value: false,
-      enabled: false
-    }
-  ]
-
   return (
     <Box pt={4} {...props}>
-      <form>
-        {items.map((item, i) => {
-          const { id, title, subtitle } = item
-          return (
-            <FormControl
-              key={id}
-              borderBottom='1px solid #A0AEC0'
-              px={4}
-              h='75px'
-            >
-              <Flex
-                flexDir='row'
-                justify='space-between'
-                align='center'
-                h='full'
-              >
-                <FormLabel
-                  htmlFor={id}
-                  display='inline-block'
-                >
-                  <Text fontWeight='bold'>
-                    {title}
-                  </Text>
+      <FormOptions />
 
-                  {subtitle && <Text
-                      fontSize='14px'
-                      color={useColorModeValue('#818692', '#c3c7e0')}
-                    >
-                      {subtitle}
-                    </Text>}
-                </FormLabel>
-
-                <Flex align='center'>
-                  <Switch
-                    id={id}
-                    variant='wordle'
-                    isChecked={item.value}
-                    onChange={item.handler}
-                    disabled={!item.enabled}
-                  />
-                </Flex>
-              </Flex>
-            </FormControl>
-          )
-        })}
-      </form>
+      <Links />
     </Box>
   )
 }
