28dffd0575573089acdb6ec683c778790092382d
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-04-10 22:00:59 -0400

    disable outline border when focus Switch chakra component

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,25 +1,21 @@
 import { extendTheme } from '@chakra-ui/react'
 
+// https://github.com/chakra-ui/chakra-ui/issues/708
+// https://github.com/chakra-ui/chakra-ui/discussions/2459
 const components = {
-  Button: {
-    baseStyle: {
-      _focus: {
-        boxShadow: 'none'
-      }
-    },
-    variants: {
-      'with-shadow': {
-        bg: 'red.400',
-        boxShadow: '0 0 2px 2px #efdfde'
-      }
-    }
+  Switch: {
+    baseStyle: { track: { _focus: { boxShadow: 'none' } } }
   }
+  // Button: {
+  //   baseStyle: { _focus: { boxShadow: 'none' } }
+  // }
 }
 
 const theme = extendTheme({
   initialColorMode: 'light',
   useSystemColorMode: true,
   components
+  // shadows: { outline: '0 0 0 0 #fff' } // disable outline borders in all components excluding Inputs
 })
 
 export default theme
