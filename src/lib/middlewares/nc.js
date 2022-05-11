8c5e04bb82951ecf7a553c19db1ffb7be42b7e12
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-11 11:44:47 -0400

    feat(api): :sparkles: add next-connect middlewares

---
+++ src/lib/middlewares/nc.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+export const onError = (error, req, res, next) => {
+  console.error(error?.stack)
+
+  if (error?.response?.data) {
+    const { status, data } = error.response
+    return res.status(status).json({ error: data })
+  }
+
+  if (error?.code === 'ECONNREFUSED') {
+    return res.status(500).json({ error: 'Could not connect to the API' })
+  }
+
+  if (error?.code === 'ECONNRESET') {
+    return res.status(500).json({ error: 'Could not connect to the API' })
+  }
+
+  if (error.message) {
+    return res.status(500).json({ error: error.message })
+  }
+
+  return res.status(500).end()
+}
+
+export const onNoMatch = (req, res) => {
+  res.status(405).json({ error: 'Method not allowed' })
+}
