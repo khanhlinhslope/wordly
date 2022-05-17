d0b503ad43fa71987f00da08a6589143bc16a3bb
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-17 18:56:05 -0400

    refactor(components): :recycle: add resolvers to define dynamic values such bg, borders, and classes

--- src/components/Game/WordleGrid/LetterBox.jsx
+++ src/components/Game/WordleGrid/LetterBox.jsx
@@ -1,7 +1,7 @@
-import { Flex, chakra, useColorModeValue, useToken } from '@chakra-ui/react'
+import { Flex, chakra, useColorModeValue } from '@chakra-ui/react'
 import useStore from '@lib/store'
 
-const checkCurrentInput = ({ wordInput, inputIndex, rowIndex, colIndex, isSubmitted }) => {
+const checkCursor = ({ wordInput, inputIndex, rowIndex, colIndex, isSubmitted }) => {
   if (isSubmitted) return false
   if (inputIndex !== rowIndex) return false
 
@@ -17,27 +17,19 @@ const checkCurrentInput = ({ wordInput, inputIndex, rowIndex, colIndex, isSubmit
   // check if is the last letter of the word
   isCurrentInput = colIndex === submittedWord.length - 1
 
-  // console.log({
-  //   submittedWord,
-  //   inputIndex,
-  //   rowIndex,
-  //   colIndex,
-  //   isSubmitted,
-  //   emptyLetterIndex,
-  //   isCurrentInput
-  // })
-
   return isCurrentInput
 }
 
-// isSubmitted is a boolean that indicates whether the entire row-word has been submitted or not
-const LetterBox = ({ letterData, isSubmitted, rowIndex, colIndex }) => {
-  const { wordInput, inputIndex } = useStore()
-  const { letter, status } = letterData
+const resolveBorderColor = ({ isSubmitted, status }) => {
   const isEmpty = status === 'empty'
+  let borderColor = 'none'
+  if (!isSubmitted && isEmpty) borderColor = useColorModeValue('blackAlpha.300', 'blackAlpha.400')
+  if (!isSubmitted && !isEmpty) borderColor = useColorModeValue('blackAlpha.500', 'blackAlpha.600')
 
-  const isCurrentInput = checkCurrentInput({ wordInput, inputIndex, rowIndex, colIndex, isSubmitted })
+  return borderColor
+}
 
+const resolveBackgroundColor = ({ isSubmitted, status }) => {
   const COLORS = {
     default: useColorModeValue('gray.100', 'gray.200'),
     guessed: useColorModeValue('green.300', 'green.400'),
@@ -45,55 +37,61 @@ const LetterBox = ({ letterData, isSubmitted, rowIndex, colIndex }) => {
     failed: useColorModeValue('gray.400', 'gray.500')
   }
 
-  const [alpha0, alpha1, alpha2, alpha3] = useToken(
-    'colors',
-    ['blackAlpha.300', 'blackAlpha.400', 'blackAlpha.500', 'blackAlpha.600']
-  )
-
-  const boxSize = [14, 14, 16, 16, 16]
-
-  const border = isSubmitted
-    ? 'none'
-    : `2px solid ${isEmpty
-      ? useColorModeValue(alpha0, alpha1)
-      : useColorModeValue(alpha2, alpha3)
-    }`
-
   let bg
   if (!isSubmitted) bg = COLORS.default
   else if (status === 'guessed') bg = COLORS.guessed
   else if (status === 'exists') bg = COLORS.exists
   else if (status === 'not_exists') bg = COLORS.failed
 
+  return bg
+}
+
+const resolveClassName = ({ isSubmitted, status, isCurrentCursor }) => {
   let className = ''
-  if (isCurrentInput) className = 'cell-type-animation'
+  if (isCurrentCursor) className = 'cell-type-animation'
   else if (isSubmitted) {
     className = `cell-reveal-animation ${status}`
   }
 
-  let animationDelay = 0
-  if (isSubmitted) animationDelay = `${colIndex * 350}ms`
+  return className
+}
 
+// isSubmitted is a boolean that indicates whether the entire row-word has been submitted or not
+const LetterBox = ({ letterData, isSubmitted, rowIndex, colIndex }) => {
+  const { wordInput, inputIndex } = useStore()
+  const { letter, status } = letterData
+
+  const boxSize = [14, 14, 16, 16, 16]
+  const border = isSubmitted ? 'none' : '2px solid'
   const fontColor = isSubmitted ? 'white' : 'black'
+  const animationDelay = isSubmitted ? `${colIndex * 350}ms` : 0
+
+  const isCurrentCursor = checkCursor({ wordInput, inputIndex, rowIndex, colIndex, isSubmitted })
+  const borderColor = resolveBorderColor({ isSubmitted, status })
+  const bg = resolveBackgroundColor({ isSubmitted, status })
+  const className = resolveClassName({ isSubmitted, status, isCurrentCursor })
 
   return (
     <Flex
       h={boxSize}
       w={boxSize}
+      bg={bg}
+      color={fontColor}
+      className={className}
+      border={border}
+      borderColor={borderColor}
+      fontWeight='bold'
+      fontSize={36}
       borderRadius={4}
       textTransform='uppercase'
       justify='center'
       align='center'
-      border={border}
-      bg={bg}
-      color={fontColor}
-      transition='all .2s ease-in-out'
-      fontWeight='bold'
-      fontSize={36}
-      className={className}
       style={{ animationDelay }}
     >
-      <chakra.span className={isSubmitted && 'letter-container'} style={{ animationDelay }}>
+      <chakra.span
+        className={isSubmitted && 'letter-container'}
+        style={{ animationDelay }}
+      >
         {letter}
       </chakra.span>
     </Flex>
