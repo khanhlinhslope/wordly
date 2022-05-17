e3b8c47c565aa83cb7fd567a237427f58634e626
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-16 23:14:56 -0400

    perf(components): :zap: remove renderer and small ui changes

--- src/components/Modals/GameOver/CountDown.jsx
+++ src/components/Modals/GameOver/CountDown.jsx
@@ -1,24 +1,22 @@
-import Countdown, { zeroPad } from 'react-countdown'
-import { chakra, Flex, Heading } from '@chakra-ui/react'
+import Countdown from 'react-countdown'
+import { Box, Flex, Text } from '@chakra-ui/react'
 import { nextWordTs } from '@lib/wotd'
-
-const renderer = ({ hours, minutes, seconds }) => {
-  return (
-    <chakra.span fontSize={32} fontWeight={600}>
-      {zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
-    </chakra.span>
-  )
-}
 
 const CountDown = ({ ...props }) => {
   const timestamp = nextWordTs()
 
   return (
-    <Flex flexDir='column' align='center' {...props}>
-      <Heading as='h3' fontSize={24} fontWeight={400}>
-        Next Wordle
-      </Heading>
-      <Countdown date={timestamp} renderer={renderer} />
+    <Flex
+      flexDir='column'
+      textAlign='center'
+      {...props}
+    >
+      <Text fontSize={20} fontWeight={600}>
+        New puzzle in
+      </Text>
+      <Box fontSize={20} fontWeight={600}>
+        <Countdown date={timestamp} daysInHours={true} />
+      </Box>
     </Flex>
   )
 }
