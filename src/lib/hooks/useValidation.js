2269884995c6471d3ad72090b89a5efa5af9dcdb
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-19 21:15:25 -0400

    perf(hooks): :zap: remove refs to which avoids duplicated toasts

--- src/lib/hooks/useValidation.js
+++ src/lib/hooks/useValidation.js
@@ -1,4 +1,3 @@
-import { useRef } from 'react'
 import { toast } from 'react-toastify'
 import dictionary from '@assets/dictionary/es/words.json'
 
@@ -8,20 +7,12 @@ const checkWord = word => {
 }
 
 const useValidation = () => {
-  // toast refs to avoid duplicated toasts
-  const toast1Id = useRef(null) // toast for empty letters
-  const toast2Id = useRef(null) // toast for non-existing word
-
   const emptyLettersToast = () => {
-    if (!toast.isActive(toast1Id.current)) {
-      toast1Id.current = toast(' Not enough letters')
-    }
+    toast(' Not enough letters')
   }
 
   const noValidWordToast = word => {
-    if (!toast.isActive(toast2Id.current)) {
-      toast2Id.current = toast(`"${word}" not exists in our dictionary`)
-    }
+    toast(`"${word}" not exists in our dictionary`)
   }
 
   const validateWord = wordObj => {
