4dadf397a648d68f7dc62d4cf6419f3f26d63774
Luis Marsiglia <marsiglia.business@gmail.com>
2023-04-29 14:11:41 -0400

    fix  errors

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,14 +1,14 @@
 /* @ref: https://github.com/garmeeh/next-seo */
 
-const title = "Wordly - A daily word-guess game";
+const title = 'Wordly - A daily word-guess game'
 const description =
-  "Guess the hidden word in 6 tries. A new puzzle is available each day.";
-const url = "https://wordly-alpha.vercel.app";
+  'Guess the hidden word in 6 tries. A new puzzle is available each day.'
+const url = 'https://wordly-alpha.vercel.app'
 const imageUrl = `https://og-image.vercel.app/${encodeURI(
   title
-)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`;
+)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`
 const keywords =
-  "wordly, wordle, word game, word guesser, word guess game, daily word game, daily word guess game, wordle en español, nytimes wordle game";
+  'wordly, wordle, word game, word guesser, word guess game, daily word game, daily word guess game, wordle en español, nytimes wordle game'
 
 const seo = {
   titleTemplate: `%s | ${title}`,
@@ -17,8 +17,8 @@ const seo = {
   openGraph: {
     description,
     title,
-    locale: "en_US",
-    type: "website",
+    locale: 'en_US',
+    type: 'website',
     url,
     canonical: url,
     images: [
@@ -26,32 +26,32 @@ const seo = {
         url: imageUrl,
         width: 800,
         height: 600,
-        alt: title,
-      },
-    ],
+        alt: title
+      }
+    ]
   },
   twitter: {
-    handle: "@marsidev",
-    site: "@marsidev",
-    cardType: "summary_large_image",
+    handle: '@marsidev',
+    site: '@marsidev',
+    cardType: 'summary_large_image'
   },
   additionalMetaTags: [
     {
-      name: "keywords",
-      content: keywords,
-    },
+      name: 'keywords',
+      content: keywords
+    }
   ],
   additionalLinkTags: [
     {
-      rel: "icon shortcut",
-      href: "/wordle-icon-32.png",
-      sizes: "3232",
+      rel: 'icon shortcut',
+      href: '/wordle-icon-32.png',
+      sizes: '3232'
     },
     {
-      rel: "apple-touch-icon",
-      href: "/wordle-icon-192.png",
-    },
-  ],
-};
+      rel: 'apple-touch-icon',
+      href: '/wordle-icon-192.png'
+    }
+  ]
+}
 
-export { seo as defaultSeo, url as defaultUrl };
+export { seo as defaultSeo, url as defaultUrl }
--- src/components/Modals/Settings/Links.jsx
+++ src/components/Modals/Settings/Links.jsx
@@ -3,44 +3,44 @@ import {
   Link,
   useColorModeValue,
   Text,
-  VStack,
-} from "@chakra-ui/react";
+  VStack
+} from '@chakra-ui/react'
 
 const Links = ({ ...props }) => {
-  const linkColor = useColorModeValue("#818692", "#c3c7e0");
-  const borderColor = useColorModeValue("gray.200", "gray.600");
+  const linkColor = useColorModeValue('#818692', '#c3c7e0')
+  const borderColor = useColorModeValue('gray.200', 'gray.600')
 
   const links = [
     {
-      id: "about",
-      title: "About",
-      urlCaption: "Twitter",
-      url: "https://twitter.com/marsidev",
+      id: 'about',
+      title: 'About',
+      urlCaption: 'Twitter',
+      url: 'https://twitter.com/marsidev'
     },
     {
-      id: "source-code",
-      title: "Source code",
-      urlCaption: "GitHub",
-      url: "https://github.com/marsidev/wordly",
-    },
-  ];
+      id: 'source-code',
+      title: 'Source code',
+      urlCaption: 'GitHub',
+      url: 'https://github.com/marsidev/wordly'
+    }
+  ]
 
   return (
     <VStack {...props}>
-      {links.map((link) => {
-        const { id, title, url, urlCaption } = link;
+      {links.map(link => {
+        const { id, title, url, urlCaption } = link
 
         return (
           <HStack
             key={id}
-            align="center"
-            borderBottom="1px solid"
+            align='center'
+            borderBottom='1px solid'
             borderColor={borderColor}
-            justify="space-between"
-            minH="75px"
-            minW={["300px", "500px"]}
+            justify='space-between'
+            minH='75px'
+            minW={['300px', '500px']}
             px={4}
-            w="100%"
+            w='100%'
           >
             <Text fontWeight={600}>{title}</Text>
 
@@ -50,16 +50,16 @@ const Links = ({ ...props }) => {
                 color={linkColor}
                 fontSize={16}
                 href={url}
-                textDecoration="underline"
+                textDecoration='underline'
               >
                 {urlCaption}
               </Link>
             </span>
           </HStack>
-        );
+        )
       })}
     </VStack>
-  );
-};
+  )
+}
 
-export default Links;
+export default Links
